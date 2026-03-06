aa8ce851e0d18af1eb262cf68bd817c4d81b9fb1
kfltchJUNO <ohejunho@gmail.com>
2026-03-07 01:30:01 +0900

    PWA 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+// @ts-ignore
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // ⭐ swcMinify는 최신 Next.js에서 기본 사양이므로 삭제했습니다.
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
